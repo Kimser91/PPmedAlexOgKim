70438dd713418ad9b1d428c8f29b5d9a8d4689b6
Kimser91 <149468829+Kimser91@users.noreply.github.com>
2024-12-13 13:02:17 +0100

    Kim

---
+++ PPmedAlexOgKim/Overview.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace PPmedAlexOgKim;
+
+public class Overview
+{
+    List<Task> myTasks = new List<Task>();
+
+    public void MainMenu()
+    {
+        Console.WriteLine("Welcome");
+
+        string Choise = Console.ReadLine();
+        switch (Choise)
+        {
+            case "1":
+                PrintList(); break;
+            case "2":
+                AddTask(); break;
+            case "3":
+                RemoveTask(); break;
+            default:
+                Environment.Exit(0);
+                break;
+        }
+    }
+
+    void PrintList()
+    {
+        foreach (var task in myTasks)
+        {
+            Console.WriteLine();
+            Console.WriteLine(task.getTitle());
+            Console.WriteLine(task.getDescription());
+            Console.WriteLine();
+            
+        }
+    }
+
+    public void AddTask()
+    {
+        string tit = "";
+
+        var NewTask = new Task();
+
+    }
+}
